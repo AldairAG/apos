6d115f825ff57a01c7ac92c217d650edcd7d554e
OrlandoRF01 <orlix.r.f@gmail.com>
2026-04-08 15:52:27 -0600

    feat: Eliminar controladores de Inventario, Material, Producción, Receta y Usuario

--- backend/src/main/java/com/api/apos/controller/UsuarioController.java
+++
@@ -1,5 +0,0 @@
-package com.api.apos.controller;
-
-public class UsuarioController {
-    
-}
--- backend/src/main/java/com/api/apos/entity/ProductoElaborado.java
+++ backend/src/main/java/com/api/apos/entity/ProductoElaborado.java
@@ -1,6 +1,5 @@
 package com.api.apos.entity;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
